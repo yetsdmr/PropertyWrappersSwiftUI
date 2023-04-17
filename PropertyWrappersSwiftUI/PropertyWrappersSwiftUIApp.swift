adecae02dd960bdcfafbf9b21a18b34fb85135b1
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-04-17 16:48:33 +0300

    Initial Commit

---
+++ PropertyWrappersSwiftUI/PropertyWrappersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersSwiftUIApp.swift
+//  PropertyWrappersSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
